87c5b7e8358096cbd2d1e10bb5a13fceed9dbb76
mohamed sameh <mohamedsameh1998@gmail.com>
2021-01-23 11:50:46 -0800

    .gitignore fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,29 @@
 package com.company;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.videoio.VideoCapture;
+import com.company.services.TimedFileConsumingService;
+
+import java.io.File;
 
 import static org.opencv.imgcodecs.Imgcodecs.imread;
 
 public class Main {
     static{ nu.pattern.OpenCV.loadLocally(); }
-    public static void main(String[] args) {
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-//        Mat src = Imgcodecs.imread("D:\\Downloads\\useless stuff\\image001.jpg");
-        VideoCapture vc = new VideoCapture("22.mp4");
-        System.out.println("Captured the stream");
-        Mat img = new Mat();
-        int frames = 0;
-        KafkaFrameWriter KFWriter = new KafkaFrameWriter();
-        long start = System.currentTimeMillis();
-        while(frames < 100) {
-            vc.read(img);
-            frames++;
-            KFWriter.writeFrame(img);
-            //HighGui.imshow("Original Image", img);
-            //HighGui.waitKey(1);
+    static class UploaderFileService extends TimedFileConsumingService {
+        public UploaderFileService(String path) {
+            super(path);
         }
-        KFWriter.closeProducer();
-        long end = System.currentTimeMillis();
-        double time = (end - start) * 1.0 / 1000;
-        System.out.println("Time: " + time + " seconds");
-        double fps = frames * 1.0 / time;
-        System.out.println("FPS: " + fps);
+
+        public UploaderFileService(String path, long delay) {
+            super(path, delay);
+        }
+
+        @Override
+        protected void execute(File file) {
+            System.out.println("Got to a file: " + file.getName());
+        }
+    }
+    public static void main(String[] args) {
+        UploaderFileService ufs = new UploaderFileService("tests", 10000);
+        ufs.run();
     }
 }
