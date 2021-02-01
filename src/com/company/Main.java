1fbbeddc815535a8d87ec1419847fa107606d7c3
mohamed sameh <mohamedsameh1998@gmail.com>
2021-02-01 18:48:42 +0200

    Add test main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,13 @@ public class Main {
     static{ nu.pattern.OpenCV.loadLocally(); }
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        RedisFrames rf = RedisFrames.getDefaultRedisFrames();
+        Mat mat = new Mat();
+        while(true) {
+            String sMat = rf.getLastFrameForCamera(Config.DEFAULTCAMERAIP, Config.DEFAULTCAMERAID);
+            mat = ImageProcessor.stringToMat(sMat);
+            HighGui.imshow("Test", mat);
+            HighGui.waitKey(10);
+        }
     }
 }
